1b65aad928b5760649f11e2dddf48389d0d7b645
cvolo4yzhka <cvolo4yzhka@mail.ru>
2019-02-15 11:15:03 +0300

    rebase

--- compile_kernel.sh
+++ compile_kernel.sh
@@ -8,7 +8,6 @@ export CROSS_COMPILE=~/Kernel/toolchain/aarch64-linux-android-4.9/bin/aarch64-li
 #set directory for build output
 export  KBUILD_OUTPUT=out
 #set defconfig
-make zte_a476_n1_defconfig
-#make fih6737m_65_n1_defconfig
+make zte_blade_a476_n1_defconfig
 #start compile
 make Image.gz-dtb -j16 2>&1 | tee out/build.log
